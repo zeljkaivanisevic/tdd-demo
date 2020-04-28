45e70f1c3712524a06db7baa01ead822c415afee
zeljkaivanisevic <57908220+zeljkaivanisevic@users.noreply.github.com>
2020-04-28 20:09:33 +0200

    Add files via upload

---
+++ src/hotel.js
@@ -0,0 +1,22 @@
+let pricelist = [
+    { from: '2020-01-01', to: '2020-02-01', price: 34.5 },
+    { from: '2020-02-02', to: '2020-03-01', price: 37.0 },
+    { from: '2020-03-02', to: '2020-05-15', price: 39.0 },
+    { from: '2020-05-16', to: '2020-06-15', price: 37.0 },
+]
+
+function logPricelist(pricelist) {
+    let ispis = ""
+    let prethodni = {}
+    pricelist.sort((a, b) => a.price - b.price)
+    
+    pricelist.forEach(function (a) {
+        if (a.price == prethodni.price) ispis =ispis + ', ' + `${a.from} do ${a.to}`
+        else ispis =ispis + '\n' + `${a.price} : ${a.from} do ${a.to}`
+        prethodni = a
+    })
+
+    console.log(ispis)
+}
+
+logPricelist(pricelist)
